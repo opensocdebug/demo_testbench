d0319ef18a78b2af719c77b8bc1e8c42ea73ba3b
Stefan Wallentowitz <stefan@wallentowitz.de>
2016-01-26 11:02:32 +0100

    Squashed 'glip/' changes from a44a1bd..0f20661
    0f20661 TCP-DPI: Fix com_rst signal
    4140000 TCP-DPI: Don't reset tcp on system rst
    
    git-subtree-dir: glip
    git-subtree-split: 0f206612da53f5e86ed5e56688ecf7537f6389dd

--- src/backend_tcp/logic/dpi/glip_tcp_toplevel.sv
+++ src/backend_tcp/logic/dpi/glip_tcp_toplevel.sv
@@ -75,7 +75,6 @@ module glip_tcp_toplevel
    
    always @(negedge clk_logic) begin
       if (rst) begin
-         glip_tcp_reset(obj);
          logic_rst = 0;
          com_rst = 0;           
       end else begin
@@ -85,9 +84,9 @@ module glip_tcp_toplevel
 
          connected = glip_tcp_connected(obj);
          if (connected > 0) begin
-            com_rst = 1'b1;
-         end else begin
             com_rst = 1'b0;
+         end else begin
+            com_rst = 1'b1;
          end
          
          state = glip_tcp_next_cycle(obj);
