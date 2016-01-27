b4389e5fe6110eb56e93d50e8b71637d35894f2e
Stefan Wallentowitz <stefan@wallentowitz.de>
2016-01-27 13:02:23 +0100

    Add all module stubs and build entire system

--- src/testbench.sv
+++ src/testbench.sv
@@ -7,13 +7,12 @@ module testbench
    glip_channel.master fifo_out
    );
 
-   /* Modules->Ring */
-   dii_channel in_ports [1:0];
-   /* Ring->Modules */
-   dii_channel out_ports [1:0];   
+   localparam N = 3;
 
-   assign in_ports[1].ready = 1;
-   assign out_ports[1].valid = 0;
+   /* Modules->Ring */
+   dii_channel in_ports [N-1:0];
+   /* Ring->Modules */
+   dii_channel out_ports [N-1:0];   
 
    osd_him
      u_him(.*,
@@ -22,18 +21,47 @@ module testbench
            .dii_out  (out_ports[0]),
            .dii_in   (in_ports[0]));
 
+   osd_scm
+     #(.VENDORID(8'h0), .SYSTEMID(8'h0), .NUM_MOD(N-1))
+   u_scm(.*,
+         .debug_in  (in_ports[1]),
+         .debug_out (out_ports[1]));
+
+   osd_dem_uart
+     u_uart (.*,
+             .debug_in  (in_ports[2]),
+             .debug_out (out_ports[2]),
+             .out_char  ('x),
+             .out_valid (0),
+             .out_ready (),
+             .in_char   (),
+             .in_valid  (),
+             .in_ready  (1));
+
+   dii_channel_flat #(.N(N)) in_flat;
+   dii_channel_flat #(.N(N)) out_flat;
+
+   genvar i;
+   generate
+      for (i = 0; i < N; i = i + 1) begin
+         assign out_flat.data[(i+1)*16-1:i*16] = out_ports[i].data;
+         assign out_flat.first[i] = out_ports[i].first;
+         assign out_flat.last[i]  = out_ports[i].last;
+         assign out_flat.valid[i] = out_ports[i].valid;
+         assign out_ports[i].ready = out_flat.ready[i];
+         assign in_ports[i].data = in_flat.data[(i+1)*16-1:i*16];
+         assign in_ports[i].first = in_flat.first[i];
+         assign in_ports[i].last = in_flat.last[i];
+         assign in_ports[i].valid = in_flat.valid[i];
+         assign in_flat.ready[i] = in_ports[i].ready;
+      end
+   endgenerate
+
+
    debug_ring
-     #(.PORTS(2))
+     #(.PORTS(N))
    u_ring(.*,
-          .in_flat_data ({out_ports[1].data, out_ports[0].data}),
-          .in_flat_first ({out_ports[1].first, out_ports[0].first}),
-          .in_flat_last ({out_ports[1].last, out_ports[0].last}),
-          .in_flat_valid ({out_ports[1].valid, out_ports[0].valid}),
-          .in_flat_ready ({out_ports[1].ready, out_ports[0].ready}),
-          .out_flat_data ({in_ports[1].data, in_ports[0].data}),
-          .out_flat_first ({in_ports[1].first, in_ports[0].first}),
-          .out_flat_last ({in_ports[1].last, in_ports[0].last}),
-          .out_flat_valid ({in_ports[1].valid, in_ports[0].valid}),
-          .out_flat_ready ({in_ports[1].ready, in_ports[0].ready}));
-   
+          .in_flat  (out_flat),
+          .out_flat (in_flat));
+
 endmodule // testbench
