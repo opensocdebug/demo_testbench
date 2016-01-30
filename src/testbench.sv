be4f022d39d25342754c3d917ca670be17055146
Stefan Wallentowitz <stefan@wallentowitz.de>
2016-01-30 14:02:51 +0100

    Change to NASTI UART interface

--- src/testbench.sv
+++ src/testbench.sv
@@ -28,55 +28,71 @@ module testbench
          .debug_in  (in_ports[1]),
          .debug_out (out_ports[1]));
 
-   logic [7:0] uart_char;
-   logic       uart_valid;
-   logic       uart_ready;
+   logic [2:0] ar_addr;
+   logic       ar_valid;
+   logic       ar_ready;
+   logic [1:0] r_resp;
+   logic [7:0] r_data;
+   logic       r_valid;
+   logic       r_ready;
+   logic [2:0] aw_addr;
+   logic       aw_valid;
+   logic       aw_ready;
+   logic [7:0] w_data;
+   logic       w_valid;
+   logic       w_ready;
+   logic [1:0] b_resp;
+   logic       b_valid;
+   logic       b_ready;
+   
    reg [4:0]   counter;
 
    always_ff @(posedge clk) begin
       if (rst) begin
          counter <= 0;
       end else begin
-         if (uart_ready & (counter < 14)) begin
+         if (w_ready & (counter < 14)) begin
             counter <= counter + 1;
          end
       end
    end
 
+   assign aw_valid = w_valid;
+   assign aw_addr = 0;
+   assign b_ready = 1;
+   
+   assign ar_valid = 0;
+   assign ar_addr = 'x;
+   assign r_ready = 0;
+   
    always_comb @(*) begin
-      uart_valid = 1;
-      uart_char = 'x;
+      w_valid = 1;
+      w_data = 'x;
       
       case (counter)
-        0: uart_char = 8'h48;
-        1: uart_char = 8'h65;
-        2: uart_char = 8'h6c;
-        3: uart_char = 8'h6c;
-        4: uart_char = 8'h6f;
-        5: uart_char = 8'h20;
-        6: uart_char = 8'h57;
-        7: uart_char = 8'h6f;
-        8: uart_char = 8'h72;
-        9: uart_char = 8'h6c;
-        10: uart_char = 8'h64;
-        11: uart_char = 8'h21;
-        12: uart_char = 8'h0a;
-        default: uart_valid = 0;        
+        0: w_data = 8'h48;
+        1: w_data = 8'h65;
+        2: w_data = 8'h6c;
+        3: w_data = 8'h6c;
+        4: w_data = 8'h6f;
+        5: w_data = 8'h20;
+        6: w_data = 8'h57;
+        7: w_data = 8'h6f;
+        8: w_data = 8'h72;
+        9: w_data = 8'h6c;
+        10: w_data = 8'h64;
+        11: w_data = 8'h21;
+        12: w_data = 8'h0a;
+        default: w_valid = 0;        
       endcase // case (counter)
    end
    
-   osd_dem_uart
+   osd_dem_uart_nasti
      u_uart (.*,
              .id (10'd2),
              .debug_in  (in_ports[2]),
-             .debug_out (out_ports[2]),
-             .out_char  (uart_char),
-             .out_valid (uart_valid),
-             .out_ready (uart_ready),
-             .in_char   (),
-             .in_valid  (),
-             .in_ready  ('1));
-
+             .debug_out (out_ports[2]));
+   
    dii_channel #(.N(N)) dii_in ();
    dii_channel #(.N(N)) dii_out ();
 
